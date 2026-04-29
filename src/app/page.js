1c841271fd06a6e8fde8ac2ec405093205d799be
webxartist <webxartist@gmail.com>
2026-04-29 18:09:53 +0530

    b

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,45 @@
-// import ContactSection from "@/Components/ContactSection";
+// // import ContactSection from "@/Components/ContactSection";
 
-import AboutSection from "@/Components/AboutSection";
-import Hero from "@/Components/Hero";
-import ProductSection from "@/Components/ProductSection";
-// import ProjectsGallery from "@/Components/ProjectsGallery";
-import TestimonialSlider from "@/Components/Testimonials";
-import WhyChooseUs from "@/Components/WhyChooseUs";
-import ContactSection from "@/Components/ContactSection";
-import ProductAbout from "@/Components/ProductAbout";
+// import AboutSection from "@/Components/AboutSection";
+// import Hero from "@/Components/Hero";
+// import ProductSection from "@/Components/ProductSection";
+// // import ProjectsGallery from "@/Components/ProjectsGallery";
+// import TestimonialSlider from "@/Components/Testimonials";
+// import WhyChooseUs from "@/Components/WhyChooseUs";
+// import ContactSection from "@/Components/ContactSection";
+// import ProductAbout from "@/Components/ProductAbout";
+// import React from "react";
+// import GSTBanner from "@/Components/GSTBanner";
+// // import QuatationMaker from "../Components/QuatationMaker";
+import Webxartist from "../Components/Webxartist";
+
+// const page = () => {
+//   return (
+//     <>
+//       <Hero />
+//       <ProductSection />
+//       <WhyChooseUs />
+//       <AboutSection />
+//       <GSTBanner />
+//       <ProductAbout />
+//       {/* <ProjectsGallery /> */}
+//       {/* <ContactSection /> */}
+//       <TestimonialSlider />
+//       <ContactSection />
+//       {/* <QuatationMaker /> */}
+//     </>
+//   );
+// };
+
+// export default page;
+
 import React from "react";
-import GSTBanner from "@/Components/GSTBanner";
-// import QuatationMaker from "../Components/QuatationMaker";
 
 const page = () => {
   return (
-    <>
-      <Hero />
-      <ProductSection />
-      <WhyChooseUs />
-      <AboutSection />
-      <GSTBanner />
-      <ProductAbout />
-      {/* <ProjectsGallery /> */}
-      {/* <ContactSection /> */}
-      <TestimonialSlider />
-      <ContactSection />
-      {/* <QuatationMaker /> */}
-    </>
+    <div>
+      <Webxartist />
+    </div>
   );
 };
 
